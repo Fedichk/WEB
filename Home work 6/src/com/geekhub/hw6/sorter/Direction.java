ddf855d09250695b7df35e109b5797313b1b9bed
Fedir kantur <fedichk@gmail.com>
2016-11-30 09:55:52 +0200

    Home work 6

--- Home work 6/src/com/geekhub/hw6/sorter/Direction.java
+++ Home work 6/src/com/geekhub/hw6/sorter/Direction.java
@@ -1,5 +1,16 @@
 package com.geekhub.hw6.sorter;
 
 public enum Direction {
-    ASC, DESC
+    ASC(1),
+    DESC(-1);
+
+    private final int i;
+
+    Direction(int i) {
+        this.i = i;
+    }
+
+    public int getValue() {
+        return i;
+    }
 }
--- Home work 6/src/com/geekhub/hw6/sorter/SorterByBuble.java
+++ Home work 6/src/com/geekhub/hw6/sorter/SorterByBuble.java
@@ -3,28 +3,16 @@ package com.geekhub.hw6.sorter;
 public class SorterByBuble implements ArraySorter {
 
     public static Comparable[] sort(Comparable[] elements, Direction direction) {
-        if (direction == Direction.ASC) {
+
             for (int a = 1; a < elements.length; a++) {
                 for (int b = 0; b < elements.length - a; b++) {
-                    if (((elements[b]).compareTo((elements[b + 1]))) > 0) {
+                    if (((elements[b]).compareTo((elements[b + 1])))*(direction.getValue()) > 0) {
                         Comparable temp = elements[b];
                         elements[b] = elements[b + 1];
                         elements[b + 1] = temp;
                     }
                 }
             }
-        }
-        if (direction == Direction.DESC) {
-            for (int a = 1; a < elements.length; a++) {
-                for (int b = 0; b < elements.length - a; b++) {
-                    if (((elements[b]).compareTo((elements[b + 1]))) < 0) {
-                        Comparable temp = elements[b];
-                        elements[b] = elements[b + 1];
-                        elements[b + 1] = temp;
-                    }
-                }
-            }
-        }
         return elements;
     }
 }
