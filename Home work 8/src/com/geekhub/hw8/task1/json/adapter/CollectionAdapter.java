9b6dbe648ec034a3c1f05095c047ad4870490784
Fedir kantur <fedichk@gmail.com>
2016-12-10 14:00:54 +0200

    Home work 8

---
+++ Home work 8/src/com/geekhub/hw8/task1/json/adapter/CollectionAdapter.java
@@ -0,0 +1,16 @@
+package com.geekhub.hw8.task1.json.adapter;
+
+import org.json.JSONException;
+
+import java.util.Collection;
+
+/**
+ * Converts all objects that extends java.util.Collections to JSONArray.
+ */
+public class CollectionAdapter implements JsonDataAdapter<Collection> {
+
+    @Override
+    public Object toJson(Collection c) throws JSONException{
+        return toJson(c);
+    }
+}
---
+++ Home work 8/src/com/geekhub/hw8/task1/json/adapter/LocalDateAdapter.java
@@ -0,0 +1,15 @@
+package com.geekhub.hw8.task1.json.adapter;
+
+import java.time.LocalDate;
+
+/**
+ * Converts object of type java.time.LocalDate to String by using ISO 8601 format
+ */
+public class LocalDateAdapter implements JsonDataAdapter<LocalDate> {
+
+    @Override
+    public Object toJson(LocalDate date) {
+        //implement me
+        return null;
+    }
+}
