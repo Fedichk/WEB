561631bcd11917a66ea2a1b0be425f215cf75252
Fedich <fedichk@gmail.com>
2017-01-07 17:08:33 +0200

    Home work 2

--- Home work 2/src/com/geekhub/hw2/figures/ShapeApplication.java
+++ Home work 2/src/com/geekhub/hw2/figures/ShapeApplication.java
@@ -1,23 +1,18 @@
 package com.geekhub.hw2.figures;
+
 import java.util.Scanner;
 
-/**
- * Created by Fedich on 30.10.2016.
- */
 public class ShapeApplication {
     public static void main(String[] args) {
-
         Integer shapeNumber = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, select the shape number: ");
-
-        for(ShapeType shape : ShapeType.values()) {
+        for (ShapeType shape : ShapeType.values()) {
             System.out.println(shape.getId() + ". " + shape.getName());
         }
-        while (shapeNumber < 1 || shapeNumber > 4  ) {
-
+        while (shapeNumber < 1 || shapeNumber > 4) {
             try {
-            shapeNumber = Integer.parseInt(sc.nextLine());
+                shapeNumber = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException e) {
             }
             if (shapeNumber > 0 && shapeNumber < 5) {
@@ -25,9 +20,7 @@ public class ShapeApplication {
             }
             System.out.println("Shape number is incorrect. Please, select the shape number:");
         }
-
         ShapeType shape = ShapeType.getById(shapeNumber);
-
         Shape figure = ShapeSelect.select(shape);
     }
 }
