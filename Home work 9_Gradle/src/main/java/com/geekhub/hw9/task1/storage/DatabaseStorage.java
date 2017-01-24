bb6364e9fff8c9157a4ed120291ac5852b035a1a
Fedich <fedichk@gmail.com>
2017-01-24 10:22:08 +0200

    Home work

---
+++ Home work 9_Gradle/src/main/java/com/geekhub/hw9/task1/storage/DatabaseStorage.java
@@ -0,0 +1,154 @@
+package com.geekhub.hw9.task1.storage;
+
+import com.geekhub.hw9.task1.objects.Entity;
+import com.geekhub.hw9.task1.objects.Ignore;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.*;
+
+/**
+ * Implementation of {@link Storage} that uses database as a storage for objects.
+ * It uses simple object type names to define target table to save the object.
+ * It uses reflection to access objects fields and retrieve data to map to database tables.
+ * As an identifier it uses field id of {@link Entity} class.
+ * Could be created only with {@link Connection} specified.
+ */
+public class DatabaseStorage implements Storage {
+
+    private Connection connection;
+
+    public DatabaseStorage(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public <T extends Entity> T get(Class<T> clazz, Integer id) throws StorageException {
+        //this method is fully implemented, no need to do anything, it's just an example
+        String sql = "SELECT * FROM " + clazz.getSimpleName().toLowerCase() + " WHERE id = " + id;
+        try (Statement statement = connection.createStatement()) {
+            List<T> result = extractResult(clazz, statement.executeQuery(sql));
+            return result.isEmpty() ? null : result.get(0);
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public <T extends Entity> List<T> list(Class<T> clazz) throws StorageException {
+        String sql = "SELECT * FROM " + clazz.getSimpleName().toLowerCase();
+        try (Statement statement = connection.createStatement()) {
+            return extractResult(clazz, statement.executeQuery(sql));
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public <T extends Entity> boolean delete(T entity) throws StorageException {
+        String sql = "DELETE FROM " + entity.getClass().getSimpleName().toLowerCase() + " WHERE id = " + entity.getId();
+        try (Statement statement = connection.createStatement()) {
+            return (statement.executeUpdate(sql) == 1);
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public <T extends Entity> void save(T entity) throws StorageException {
+        if (entity.isNew()) {
+            try {
+                Collection values = prepareEntity(entity).values();
+                StringBuilder preparedString = new StringBuilder();
+                for (Object value : values) {
+                    preparedString.append("?, ");
+                }
+                preparedString.delete(preparedString.length() - 2, preparedString.length());
+                String sql = String.format("INSERT INTO %s VALUES (default, %s)",
+                        entity.getClass().getSimpleName().toLowerCase(),
+                        preparedString.toString());
+                try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                    int i = 1;
+                    for (Object value : values) {
+                        ps.setObject(i++, value);
+                    }
+                    ps.executeUpdate();
+                    ResultSet generatedKeys = ps.getGeneratedKeys();
+                    generatedKeys.next();
+                    entity.setId(generatedKeys.getInt(1));
+                }
+            } catch (Exception e) {
+                throw new StorageException(e);
+            }
+        } else {
+            try {
+                Collection values = prepareEntity(entity).values();
+                Set keys = prepareEntity(entity).keySet();
+                StringBuilder preparedString = new StringBuilder();
+                for (Object key : keys) {
+                    preparedString.append(key);
+                    preparedString.append(" = ?, ");
+                }
+                preparedString.delete(preparedString.length() - 2, preparedString.length());
+                String sql = String.format("UPDATE %s SET %s WHERE id = %d",
+                        entity.getClass().getSimpleName().toLowerCase(),
+                        preparedString.toString(),
+                        entity.getId());
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    int i = 1;
+                    for (Object value : values) {
+                        ps.setObject(i++, value);
+                    }
+                    ps.executeUpdate();
+                }
+            } catch (Exception e) {
+                throw new StorageException(e);
+            }
+        }
+    }
+
+    //converts object to map, could be helpful in save method
+    private <T extends Entity> Map<String, Object> prepareEntity(T entity) throws IllegalAccessException {
+        Map<String, Object> result = new LinkedHashMap<>();
+        Field[] fields = entity.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (!field.isAnnotationPresent(Ignore.class)) {
+                result.put(field.getName(), field.get(entity));
+            }
+        }
+        return result;
+    }
+
+    //creates list of new instances of clazz by using data from resultset
+    private <T extends Entity> List<T> extractResult(Class<T> clazz, ResultSet resultSet) throws Exception {
+        List<T> result = new ArrayList<>();
+        while (resultSet.next()) {
+            T object = (T) clazz.newInstance();
+            Field[] fields = clazz.getDeclaredFields();
+            object.setId(resultSet.getInt(1));
+            int i = 2;
+            for (Field field : fields) {
+                field.setAccessible(true);
+                if (!field.isAnnotationPresent(Ignore.class)) {
+                    field.set(object, resultSet.getObject(i++));
+                }
+            }
+            result.add(object);
+        }
+        return result;
+    }
+
+    @Override
+    public <T extends Entity> int delete(Class<T> clazz) throws StorageException {
+        String sql = "DELETE FROM " + clazz.getSimpleName().toLowerCase();
+        try (Statement statement = connection.createStatement()) {
+            return (statement.executeUpdate(sql));
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
+    }
+}
---
+++ Home work 9_Gradle/src/test/java/DatabaseStorageTest.java
@@ -0,0 +1,118 @@
+import com.geekhub.hw9.task1.objects.Cat;
+import com.geekhub.hw9.task1.storage.DatabaseStorage;
+import com.geekhub.hw9.task1.storage.Storage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DatabaseStorageTest {
+
+    Connection createConn() throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:h2:~/test",
+                "sa",
+                "");
+        return connection;
+    }
+
+    @Before
+    public void createTable() throws Exception {
+        Connection conn = createConn();
+        String test = "CREATE TABLE cat" +
+                "(ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL," +
+                "NAME VARCHAR NOT NULL," +
+                "AGE INT NOT NULL)";
+
+        String insert = "INSERT INTO cat VALUES(1, 'Murka', 5);" +
+                "INSERT INTO cat VALUES(2, 'Barsik', 2);" +
+                "INSERT INTO cat VALUES(3, 'Pushistik', 4);" +
+                "INSERT INTO cat VALUES(4, 'Tom', 1);";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(test);
+            stmt.executeUpdate(insert);
+        }
+        conn.close();
+    }
+
+    @After
+    public void deleteTable() throws Exception {
+        Connection conn = createConn();
+        String delete = "DROP TABLE IF EXISTS cat;";
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(delete);
+        }
+        conn.close();
+    }
+
+    @Test
+    public void getExactlyWhatWeWantTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Assert.assertEquals(Cat.class, base.get(Cat.class, 1).getClass());
+        Assert.assertEquals("Murka", base.get(Cat.class, 1).getName());
+        Assert.assertEquals(5, base.get(Cat.class, 1).getAge());
+    }
+
+    @Test
+    public void getWhatIsNotExistsTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Assert.assertNull(base.get(Cat.class, 100));
+    }
+
+    @Test
+    public void listExactlyWhatWeWantTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Assert.assertEquals(4, base.list(Cat.class).size());
+        Assert.assertEquals("Tom", base.list(Cat.class).get(3).getName());
+    }
+
+    @Test
+    public void listIsEmptyFromEmptyTableTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        base.delete(Cat.class);
+        Assert.assertTrue(base.list(Cat.class).isEmpty());
+    }
+
+    @Test
+    public void deleteAllRowsTableTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Assert.assertEquals(4, base.delete(Cat.class));
+        Assert.assertEquals(0, base.delete(Cat.class));
+    }
+
+    @Test
+    public void saveToBaseNewObjectTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Cat cat = new Cat();
+        cat.setName("Pushok");
+        cat.setAge(8);
+        base.save(cat);
+        Assert.assertEquals("Pushok", base.get(Cat.class, 5).getName());
+    }
+
+    @Test
+    public void updateBaseWithNewNameOfExistingObjectTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Cat cat = new Cat();
+        cat.setName("Puhlyachok");
+        cat.setId(4);
+        base.save(cat);
+        Assert.assertEquals("Puhlyachok", base.get(Cat.class, 4).getName());
+    }
+
+    @Test
+    public void deleteSimpleObjectFromBaseTest() throws Exception {
+        Storage base = new DatabaseStorage(createConn());
+        Cat cat = new Cat();
+        cat.setName("Jorik");
+        cat.setAge(7);
+        base.save(cat);
+        Assert.assertTrue(base.delete(cat));
+    }
+}
