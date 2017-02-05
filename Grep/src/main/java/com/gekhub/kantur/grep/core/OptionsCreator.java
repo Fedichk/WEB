562d8d3fec17ae453bc40422be2b9fa0a9d4e3cb
Fedich <fedichk@gmail.com>
2017-02-05 13:52:12 +0200

    Home work

--- Grep/src/main/java/com/gekhub/kantur/grep/core/OptionsCreator.java
+++ Grep/src/main/java/com/gekhub/kantur/grep/core/OptionsCreator.java
@@ -7,11 +7,11 @@ public class OptionsCreator {
     private Options options = new Options();
 
     public OptionsCreator() {
-        this.options.addOption("w", "word", true, "Word to search");
-        this.options.addOption("r", "regex", true, "Regular expression to search");
-        this.options.addOption("f", "file", true, "File as source of the text");
-        this.options.addOption("u", "url", true, "URL address as source of the text");
-        this.options.addOption("h", "help", false, "Shows this help");
+        options.addOption("w", "word", true, "Word to search");
+        options.addOption("r", "regex", true, "Regular expression to search");
+        options.addOption("f", "file", true, "File as source of the text");
+        options.addOption("u", "url", true, "URL address as source of the text");
+        options.addOption("h", "help", false, "Shows this help");
     }
 
     public Options getOptions() {
